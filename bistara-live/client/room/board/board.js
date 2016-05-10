b28b02c06aa2d038212a761cd6b712199f5e17d1
Harry Rein <harryrein@Harrys-MacBook-Air.local>
2016-05-10 17:10:15 -0400

    updates to https

--- bistara-live/client/room/board/board.js
+++ bistara-live/client/room/board/board.js
@@ -108,7 +108,7 @@ if(Meteor.isClient){
         });
     });
 
-    $(document).on('input', "#upload-file", function(e){
+    $(document).on('click', "#upload-file", function(e){
         console.log("input");
         var x = document.getElementById("upload-file");
         var canvas = document.getElementById("main-whiteboard");
@@ -125,6 +125,32 @@ if(Meteor.isClient){
         if(a <= b) return a;
         return b;
     };
+
+    $(document).on('click','#post-file', function(e){
+        e.preventDefault();
+        console.log("input form");
+        var file = document.getElementById("upload-file").files[0];
+        console.log(file);
+        obtainCanvasLock("file");
+        var canvas = document.getElementById("main-whiteboard");
+        var ctx = canvas.getContext("2d");
+
+        var FR= new FileReader();
+        FR.onload = function(e) {
+            var image = new Image();
+            image.src       = e.target.result;
+            image.onload = function() {
+                ctx.drawImage(image, 0, 0, min(canvas.width, image.width),min(canvas.height, image.height));
+                console.log("calling meteor method");
+                Meteor.call("set_board_state", Session.get("pin"), canvas.toDataURL(), function(err, result){
+                    if(err) console.log(err);
+                    releaseCanvasLock();
+                });
+            };
+        };
+        FR.readAsDataURL(file);
+    });
+
 
     Template.board.events({
         'submit form':function(e){
