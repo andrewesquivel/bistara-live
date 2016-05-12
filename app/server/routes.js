41b1af24bc0c6beb31168167522fc3a33479e251
andrewesquivel <drewesquivel77@gmail.com>
2016-05-11 21:36:33 -0400

    back to specifics

--- app/server/routes.js
+++ app/server/routes.js
@@ -130,6 +130,6 @@ JsonRoutes.setResponseHeaders({
     "Cache-Control": "no-store",
     "Pragma": "no-cache",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*", //"GET, PUT, POST, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "*" //Content-Type, Authorization, X-Requested-With"
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
 });
