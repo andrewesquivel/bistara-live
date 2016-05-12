a26f862ccb0d41d714e3c300301302c9bf7dce8a
andrewesquivel <drewesquivel77@gmail.com>
2016-05-11 21:31:09 -0400

    new method

--- app/server/routes.js
+++ app/server/routes.js
@@ -111,11 +111,25 @@ Meteor.methods({
     }
 });
 
-//JsonRoutes.ErrorMiddleware.use(RestMiddleware.handleErrorAsJson);
-//JsonRoutes.setResponseHeaders({
-//    "Cache-Control": "no-store",
-//    "Pragma": "no-cache",
-//    "Access-Control-Allow-Origin": "*",
-//    "Access-Control-Allow-Methods": "*", //"GET, PUT, POST, DELETE, OPTIONS",
-//    "Access-Control-Allow-Headers": "*" //Content-Type, Authorization, X-Requested-With"
-//});
+var options = {
+    url: 'create/room',
+    getArgsFromRequest: function(request){
+        console.log("------------GET ARGS FROM REQUST------------");
+        console.log(request);
+       return [];
+    },
+    httpMethod:"GET"
+};
+
+Meteor.method("create_room_api", function(callback){
+    return createRoom();
+}, options);
+
+JsonRoutes.ErrorMiddleware.use(RestMiddleware.handleErrorAsJson);
+JsonRoutes.setResponseHeaders({
+    "Cache-Control": "no-store",
+    "Pragma": "no-cache",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*", //"GET, PUT, POST, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "*" //Content-Type, Authorization, X-Requested-With"
+});
