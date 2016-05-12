5d0487eca32b89a4420e358e40262f478ed7d55e
andrewesquivel <drewesquivel77@gmail.com>
2016-05-11 21:25:33 -0400

    ugghghghh

--- app/server/routes.js
+++ app/server/routes.js
@@ -112,10 +112,10 @@ Meteor.methods({
 });
 
 //JsonRoutes.ErrorMiddleware.use(RestMiddleware.handleErrorAsJson);
-JsonRoutes.setResponseHeaders({
-    "Cache-Control": "no-store",
-    "Pragma": "no-cache",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*", //"GET, PUT, POST, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "*" //Content-Type, Authorization, X-Requested-With"
-});
+//JsonRoutes.setResponseHeaders({
+//    "Cache-Control": "no-store",
+//    "Pragma": "no-cache",
+//    "Access-Control-Allow-Origin": "*",
+//    "Access-Control-Allow-Methods": "*", //"GET, PUT, POST, DELETE, OPTIONS",
+//    "Access-Control-Allow-Headers": "*" //Content-Type, Authorization, X-Requested-With"
+//});
